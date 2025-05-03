0973b5221dd41bd885c02d1c241479edd30ec4b1
Tiffanywanwan <147122759+Tiffanywanwan@users.noreply.github.com>
2025-05-04 03:41:21 +0800

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,9 @@
-// db.js
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: 'Tiffany20041102',
+  user: 'root', //請輸自己的密碼
+  password: '你的密碼', //請輸自己的密碼
   database: 'dbms-project',
   waitForConnections: true,
   connectionLimit: 10,
