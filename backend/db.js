b7cd3b196c37e7bc2379622896436171079fb192
Alina <alinafan888@gmail.com>
2025-05-25 01:48:36 +0800

    新增：社產功能

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root', //請輸自己的密碼
-  password: '0709', //請輸自己的密碼
+  password: '', //請輸自己的密碼
   database: 'dbms-project',
   waitForConnections: true,
   connectionLimit: 10,
