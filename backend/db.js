7f5f9789a6e42f7f0e9d5e878e8af3feaf68e005
Esther <112306066@g.nccu.edu.tw>
2025-05-26 21:35:15 +0800

    改成自己的密碼

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root', //請輸自己的密碼
-  password: '', //請輸自己的密碼
+  password: 'root', //請輸自己的密碼
   database: 'dbms-project',
   waitForConnections: true,
   connectionLimit: 10,
